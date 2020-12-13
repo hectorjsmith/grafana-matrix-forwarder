03ed7612806f32be3cbe42bc438e07780a816525
Hector <dev@hsmith.org>
2020-12-13 11:38:45 +0000

    refactor: move server handlers to new file
    Move the server request handler code to a new file to keep the main
    `server` file more focused on setting up the server.

---
+++ server/handler.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"grafana-matrix-forwarder/grafana"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func (server Server) handleGrafanaAlert(response http.ResponseWriter, request *http.Request) error {
+	bodyBytes, err := getRequestBodyAsBytes(request)
+	if err != nil {
+		return err
+	}
+	if server.appSettings.LogPayload {
+		logPayload(request, bodyBytes)
+	}
+
+	roomID, err := getRoomIDFromURL(request)
+	if err != nil {
+		return err
+	}
+
+	alert, err := getAlertPayloadFromRequestBody(bodyBytes)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("alert received (%s) - forwarding to room: %s", alert.FullRuleID(), roomID)
+
+	err = grafana.ForwardAlert(server.matrixWriteCloser.GetWriter(), roomID, alert, server.appSettings.ResolveMode)
+	if err != nil {
+		return err
+	}
+
+	response.WriteHeader(200)
+	_, err = response.Write([]byte("OK"))
+	return err
+}
+
+func logPayload(request *http.Request, bodyBytes []byte) {
+	log.Printf("%s request received at URL: %s", request.Method, request.URL.String())
+	body := string(bodyBytes)
+	fmt.Println(body)
+}
+
+func getRoomIDFromURL(request *http.Request) (string, error) {
+	roomIds, ok := request.URL.Query()["roomId"]
+	if !ok || len(roomIds[0]) < 1 {
+		return "", fmt.Errorf("url param 'roomId' is missing")
+	}
+	return roomIds[0], nil
+}
+
+func getAlertPayloadFromRequestBody(bodyBytes []byte) (alertPayload grafana.AlertPayload, err error) {
+	err = json.Unmarshal(bodyBytes, &alertPayload)
+	return
+}
+
+func getRequestBodyAsBytes(request *http.Request) ([]byte, error) {
+	return ioutil.ReadAll(request.Body)
+}
--- server/server.go
+++ server/server.go
@@ -2,12 +2,9 @@ package server
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"grafana-matrix-forwarder/cfg"
-	"grafana-matrix-forwarder/grafana"
 	"grafana-matrix-forwarder/matrix"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -78,58 +75,4 @@ func (server Server) Start() (err error) {
 		err = nil
 	}
 	return
-}
-
-func (server Server) handleGrafanaAlert(response http.ResponseWriter, request *http.Request) error {
-	bodyBytes, err := getRequestBodyAsBytes(request)
-	if err != nil {
-		return err
-	}
-	if server.appSettings.LogPayload {
-		logPayload(request, bodyBytes)
-	}
-
-	roomID, err := getRoomIDFromURL(request)
-	if err != nil {
-		return err
-	}
-
-	alert, err := getAlertPayloadFromRequestBody(bodyBytes)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("alert received (%s) - forwarding to room: %s", alert.FullRuleID(), roomID)
-
-	err = grafana.ForwardAlert(server.matrixWriteCloser.GetWriter(), roomID, alert, server.appSettings.ResolveMode)
-	if err != nil {
-		return err
-	}
-
-	response.WriteHeader(200)
-	_, err = response.Write([]byte("OK"))
-	return err
-}
-
-func logPayload(request *http.Request, bodyBytes []byte) {
-	log.Printf("%s request received at URL: %s", request.Method, request.URL.String())
-	body := string(bodyBytes)
-	fmt.Println(body)
-}
-
-func getRoomIDFromURL(request *http.Request) (string, error) {
-	roomIds, ok := request.URL.Query()["roomId"]
-	if !ok || len(roomIds[0]) < 1 {
-		return "", fmt.Errorf("url param 'roomId' is missing")
-	}
-	return roomIds[0], nil
-}
-
-func getAlertPayloadFromRequestBody(bodyBytes []byte) (alertPayload grafana.AlertPayload, err error) {
-	err = json.Unmarshal(bodyBytes, &alertPayload)
-	return
-}
-
-func getRequestBodyAsBytes(request *http.Request) ([]byte, error) {
-	return ioutil.ReadAll(request.Body)
 }
