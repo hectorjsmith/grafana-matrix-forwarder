37572f3196166012533a5429d10cdee6a23f7a5f
Hector <dev@hsmith.org>
2021-03-13 18:29:07 +0000

    feat: new -env cli flag to use environment variables (#27)
    Add a new `-env` CLI flag to ignore all other CLI flags and read all app
    configuration from environment variables. This improves support for running
    this tool in a docker environment.
    Update the docker startup script to use the new `-env` flag instead of
    converting environment variables to CLI flags.

--- docker/run.sh
+++ docker/run.sh
@@ -1,32 +1,8 @@
 #/bin/sh
 
-serverHostOpt="-host $GMF_SERVER_HOST"
-if [ -z $GMF_SERVER_HOST ]; then
-  serverHostOpt=""
-fi
-portHostOpt="-port $GMF_SERVER_PORT"
-if [ -z $GMF_SERVER_PORT ]; then
-  portHostOpt=""
-fi
-resolveModeOpt="-resolveMode $GMF_RESOLVE_MODE"
-if [ -z $GMF_RESOLVE_MODE ]; then
-  resolveModeOpt=""
-fi
-logPayloadOpt="-logPayload"
-if [ -z $GMF_LOG_PAYLOAD ] || [ $GMF_LOG_PAYLOAD = "no" ] || [ $GMF_LOG_PAYLOAD = "false" ]; then
-  logPayloadOpt=""
-fi
-
 # Print version to logs for debugging purposes
 /app/grafana-matrix-forwarder -version
 
 # Start the forwarder (use exec to support graceful shutdown)
 # Inspired by: https://akomljen.com/stopping-docker-containers-gracefully/
-exec /app/grafana-matrix-forwarder \
-    -user $GMF_MATRIX_USER \
-    -password $GMF_MATRIX_PASSWORD \
-    -homeserver $GMF_MATRIX_HOMESERVER \
-    $portHostOpt \
-    $serverHostOpt \
-    $resolveModeOpt \
-    $logPayloadOpt
+exec /app/grafana-matrix-forwarder -env
