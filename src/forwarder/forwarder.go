04074927f041ae576397a5e52f10d25eb67abc64
Hector <dev@hsmith.org>
2021-06-18 07:26:03 +0100

    refactor: split forwarder code
    Split the forwarder code into two files. One file handles forwarding
    data from Grafana to Matrix, while the other handles formatting messages
    for Matrix. This separation makes the code easier to work with.

--- src/forwarder/forwarder.go
+++ src/forwarder/forwarder.go
@@ -1,18 +1,13 @@
 package forwarder
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"grafana-matrix-forwarder/cfg"
 	"grafana-matrix-forwarder/grafana"
 	"grafana-matrix-forwarder/matrix"
-	htmlTemplate "html/template"
 	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
-	textTemplate "text/template"
 )
 
 type AlertForwarder struct {
@@ -26,34 +21,6 @@ type sentMatrixEvent struct {
 	EventID           string
 	SentFormattedBody string
 }
-
-type alertMessageData struct {
-	MetricRounding int
-	StateStr       string
-	StateEmoji     string
-	Payload        grafana.AlertPayload
-}
-
-const (
-	alertMessageTemplateStr = `{{ .StateEmoji }} <b>{{ .StateStr }}</b>
-{{- with .Payload }}<p>Rule: <a href="{{ .RuleURL }}">{{ .RuleName }}</a> | {{ .Message }}</p>
-{{- if gt (len .EvalMatches) 0 }}<ul>{{ range $match := .EvalMatches }}<li><b>{{ .Metric }}</b>: {{ RoundValue .Value $.MetricRounding }}</li>{{ end }}</ul>{{ end }}
-{{- if gt (len .Tags) 0 }}<p>Tags:</p><ul>{{ range $tagKey, $tagValue := .Tags }}<li><b>{{ $tagKey }}</b>: {{ $tagValue }}</li>{{ end }}</ul>{{ end }}{{ end }}`
-	resolvedReactionStr    = `✅`
-	resolveReplyStr        = "<mx-reply><blockquote>{{ . }}</blockquote></mx-reply>💚 ️<b>RESOLVED</b>"
-	resolveReplyPlainStr   = `💚 ️RESOLVED`
-	sentMatrixEventMapFile = "grafanaToMatrixMap.json"
-)
-
-var (
-	htmlTagRegex       = regexp.MustCompile(`<.*?>`)
-	htmlParagraphRegex = regexp.MustCompile(`</?p>`)
-
-	alertMessageTemplate = htmlTemplate.Must(htmlTemplate.New("alertMessage").Funcs(htmlTemplate.FuncMap{
-		"RoundValue": roundMetricValue,
-	}).Parse(alertMessageTemplateStr))
-	resolveReplyTemplate = textTemplate.Must(textTemplate.New("resolveReply").Parse(resolveReplyStr))
-)
 
 func NewForwarder(appSettings cfg.AppSettings, writer matrix.Writer) *AlertForwarder {
 	forwarder := &AlertForwarder{
@@ -114,58 +81,6 @@ func (forwarder *AlertForwarder) sendRegularMessage(roomID string, alert grafana
 		forwarder.saveForwardMap()
 	}
 	return
-}
-
-func buildFormattedMessageBodyFromAlert(alert grafana.AlertPayload, settings cfg.AppSettings) (message string, err error) {
-	var messageData = alertMessageData{
-		StateStr:       "UNKNOWN",
-		StateEmoji:     "❓",
-		MetricRounding: settings.MetricRounding,
-		Payload:        alert,
-	}
-	switch alert.State {
-	case grafana.AlertStateAlerting:
-		messageData.StateStr = "ALERT"
-		messageData.StateEmoji = "💔"
-	case grafana.AlertStateResolved:
-		messageData.StateStr = "RESOLVED"
-		messageData.StateEmoji = "💚"
-	case grafana.AlertStateNoData:
-		messageData.StateStr = "NO DATA"
-		messageData.StateEmoji = "❓"
-	default:
-		log.Printf("alert received with unknown state: %s", alert.State)
-	}
-	return executeAlertTemplate(alertMessageTemplate, messageData)
-}
-
-// stripHtmlTagsFromString removes all the HTML tags from an input string.
-func stripHtmlTagsFromString(input string) string {
-	bodyWithoutParagraphs := htmlParagraphRegex.ReplaceAllString(input, " ")
-	plainBody := htmlTagRegex.ReplaceAllString(bodyWithoutParagraphs, "")
-	return plainBody
-}
-
-func roundMetricValue(rawValue float64, metricRounding int) string {
-	var format string
-	if metricRounding >= 0 {
-		format = fmt.Sprintf("%%.%df", metricRounding)
-	} else {
-		format = "%v"
-	}
-	return fmt.Sprintf(format, rawValue)
-}
-
-func executeAlertTemplate(template *htmlTemplate.Template, data alertMessageData) (string, error) {
-	buffer := new(bytes.Buffer)
-	err := template.Execute(buffer, data)
-	return buffer.String(), err
-}
-
-func executeTextTemplate(template *textTemplate.Template, content string) (string, error) {
-	buffer := new(bytes.Buffer)
-	err := template.Execute(buffer, content)
-	return buffer.String(), err
 }
 
 func (forwarder *AlertForwarder) prePopulateAlertMap() {
