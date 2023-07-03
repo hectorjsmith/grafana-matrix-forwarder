b80bca951caae44b480360d39cbbd27b1b212345
Hector <hector@hjs.dev>
2023-07-03 16:27:09 +0000

    chore: restructure project folder (!72)
    * Move code up to the root folder
    * Remove CHANGELOG file (will be replaced with notes on the actual release)
    * Remove unnecessary files
    
    https://gitlab.com/hectorjsmith/grafana-matrix-forwarder/-/merge_requests/72

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-# Print version to logs for debugging purposes
-/app/grafana-matrix-forwarder -version
-
-# Start the forwarder (use exec to support graceful shutdown)
-# Inspired by: https://akomljen.com/stopping-docker-containers-gracefully/
-exec /app/grafana-matrix-forwarder -env
