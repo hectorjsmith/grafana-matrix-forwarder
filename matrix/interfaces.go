c69ec3498806e78256fb2d43298e351677c098ac
Hector <hector@hjs.dev>
2023-07-05 22:02:21 +0100

    update code docs

--- matrix/interfaces.go
+++ matrix/interfaces.go
@@ -9,10 +9,10 @@ type WriteCloser interface {
 }
 
 type Writer interface {
-	// Send a message payload to a given room and get back the response data
+	// Send a message payload to a given room and get back the response data, returns the event ID if successful
 	Send(roomID string, body string, formattedBody string) (string, error)
-	// Reply to the provided event ID with the provided plain text and formatted body
+	// Reply to the provided event ID with the provided plain text and formatted body, returns the event ID if successful
 	Reply(roomID string, eventID string, body string, formattedBody string) (string, error)
-	// React to a given message
+	// React to a given message, returns the event ID if successful
 	React(roomID string, eventID string, reaction string) (string, error)
 }
